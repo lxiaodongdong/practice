bb51e1a7f2a524b0d0f2e38f1d854c76a4b033d4
lxiaodongdong <1345720184@qq.com>
2018-01-30 08:52:56 +0800

    commit

---
+++ ListNode.h
@@ -0,0 +1,17 @@
+#pragma once
+
+typename int Rank;
+#define ListNodePosi(T) ListNode<T> *;
+
+template < typename T> 
+struct ListNode{
+	T data;
+	ListNodePosi(T) pred;
+	ListNodePosi(T) succ;
+	ListNode(){};
+	ListNode(T e,ListNodePosi(T) p = NULL, ListNodePosi(T) s = NULL):data(e),pred(p),succ(s){};
+	ListNodePosi(T) insertAsPred(T const & e);
+	ListNodePosi(T) insertAsSucc(T const & e);
+	~ListNode(){};
+};
+#include "listnode_implementation.h"
